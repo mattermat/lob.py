2a762ad149550199927040c9f09226fa85892985
Mattia Terenzi <m.terenzi92@gmail.com>
2026-04-11 15:38:27 +0200

    bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_file(fname):
 
 setup(
     name="lobpy",
-    version="1.2.0",
+    version="2.0.0",
     author="Mattia",
     description="Limit Order Book in Python",
     long_description=read_file("README.md"),
